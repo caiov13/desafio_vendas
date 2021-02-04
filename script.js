5799dc40f31e1e6261cb10c4ec83995a889abfba
Caio Vinicius <aio_vinicius11111@hotmail.com>
2021-02-04 11:35:20 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const themeButton = document.getElementById('theme_button')
+const darkTheme = 'dark_theme'
+const iconTheme = 'bx-toggle-right'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-toggle-left' : 'bx-toggle-right'
+
+
+if (selectedTheme) {
+
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'bx-toggle-left' ? 'add' : 'remove'](iconTheme)
+}
+
+
+themeButton.addEventListener('click', () => {
+   
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
